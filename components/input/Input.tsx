e596a3ffa0b55324e1a27f08b282d989176de638
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2025-01-19 13:56:14 +0200

    improve inputs usability

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -1,4 +1,5 @@
-import { View, Text, TextInput, TextInputProps } from 'react-native';
+import { View, Text, TextInput, TextInputProps, Pressable } from 'react-native';
+import React, { useRef } from 'react';
 
 import { colors } from '@/constants/colors';
 
@@ -8,21 +9,30 @@ export interface InputProps extends TextInputProps {
 }
 
 export function Input({ label, rightElement, ...props }: InputProps) {
+  const inputRef = useRef<TextInput>(null);
+
+  function handleContainerPress() {
+    inputRef.current?.focus();
+  }
+
   return (
-    <View className="w-full gap-1">
-      <Text className="text-sm text-neutral-700 font-semibold">{label}</Text>
-      <View className="w-full h-10 rounded-[10px] bg-white border border-neutral-300 flex-row items-center">
-        <TextInput
-          className="flex-1 min-w-0 px-3 h-full rounded-[10px]"
-          placeholderTextColor={colors.neutral[500]}
-          {...props}
-        />
-        {rightElement && (
-          <Text className="text-neutral-500 ml-1 mr-3 flex-shrink-0 absolute right-0 pointer-events-none">
-            {rightElement}
-          </Text>
-        )}
+    <Pressable onPress={handleContainerPress}>
+      <View className="w-full gap-1">
+        <Text className="text-sm text-neutral-700 font-semibold">{label}</Text>
+        <View className="w-full h-10 rounded-[10px] bg-white border border-neutral-300 flex-row items-center">
+          <TextInput
+            ref={inputRef}
+            className="flex-1 min-w-0 px-3 h-full rounded-[10px]"
+            placeholderTextColor={colors.neutral[500]}
+            {...props}
+          />
+          {rightElement && (
+            <Text className="text-neutral-500 ml-1 mr-3 flex-shrink-0 absolute right-0 pointer-events-none">
+              {rightElement}
+            </Text>
+          )}
+        </View>
       </View>
-    </View>
+    </Pressable>
   );
 }
