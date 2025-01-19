0230e34328305ccaed526fc728d481da81a0a0cf
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2025-01-19 13:59:52 +0200

    minor layout improvements

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,21 +3,24 @@ import { Stack } from 'expo-router';
 import HouseIcon from '@/assets/icons/house.svg';
 import { colors } from '@/constants/colors';
 import { HeaderTitle } from '@/components/HeaderTitle';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 
 // TODO: implement a different layout for web if we deploy there...
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.primary[900],
-        },
-        headerTintColor: colors.neutral[100],
-        headerTitle: () => <HeaderTitle Icon={HouseIcon}>Home Loan Calculator</HeaderTitle>,
-        animation: 'simple_push',
-        animationDuration: 250,
-      }}
-    />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.primary[900],
+          },
+          headerTintColor: colors.neutral[100],
+          headerTitle: () => <HeaderTitle Icon={HouseIcon}>Home Loan Calculator</HeaderTitle>,
+          animation: 'simple_push',
+          animationDuration: 250,
+        }}
+      />
+    </TouchableWithoutFeedback>
   );
 }
--- components/BottomBanner.tsx
+++ components/BottomBanner.tsx
@@ -7,7 +7,7 @@ interface BottomBannerProps {
 
 export function BottomBanner({ label, value }: BottomBannerProps) {
   return (
-    <View className="absolute bottom-0 left-0 right-0 bg-primary-900 p-3 pb-6 justify-between items-center gap-[18px] rounded-t-[30px]">
+    <View className="absolute bottom-0 left-0 right-0 bg-primary-900 p-4 pb-8 justify-between items-center gap-3 rounded-t-[30px]">
       <Text className="text-white font-normal">{label}</Text>
       <Text className="text-white text-lg font-semibold">{value}</Text>
     </View>
